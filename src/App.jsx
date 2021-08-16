3c279554c0cd59a6f1248093edc7fa612ea00c92
dobkir <dobkir@gmail.com>
2021-08-16 20:02:11 +0400

    Created fetch post request to the facke-server https://api.jsonbin.io/v3/b and JSON Outputting form data to the page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,38 @@ import "./App.css";
 
 function App() {
 
+  const onSubmit = (event) => {
+    event.preventDefault();
+    fetch('https://api.jsonbin.io/v3/b', {
+      method: 'POST',
+      body: JSON.stringify({ formData }),
+      headers: new Headers({
+        'Content-Type': 'application/json',
+        'X-Master-Key': '$2b$10$65lTDH3r4YCHqsLIzTJGZ.fIzJmJPTL24xO2Ol3sH9.saBA4cwWLW'
+      })
+    })
+      .then(response => {
+        // Let's check the response code
+        if (!response.ok) {
+          // If the server returned a response code out of range [200, 299]
+          return Promise.reject(new Error(
+            'Response failed: ' + response.status + ' (' + response.statusText + ')'
+          ));
+        }
+        // Next, we will only use JSON from the response body
+        return response.json();
+      })
+      // Let's check what form data was submitted to the server (remove it in the work version)
+      .then(data => {
+        alert(
+          "The server received the following data from you: \n" +
+          data.record.formData.map(field => {
+            return `${field}; \n`;
+          }).join(''));
+      })
+      .catch(error => alert(`Oops, any problem here: ${error.name}. ${error.message}`));
+  };
+
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -20,6 +52,12 @@ function App() {
 
   const [formValid, setFormValid] = useState(false);
 
+  const [formData, setFormData] = useState([]);
+  const getCurrentData = (currentValue) => {
+    return setFormData(prevData => [...prevData, currentValue]);
+  }
+
+
   useEffect(() => {
     if (usernameError || emailError || passwordError || repeatPasswordError) {
       setFormValid(false);
@@ -33,6 +71,7 @@ function App() {
     if (event.target.value.length > 14) {
       setUsernameError("Username must be less than fourteen characters")
     } else {
+      getCurrentData(event.target.value);
       setUsernameError("");
     }
   }
@@ -43,6 +82,7 @@ function App() {
     if (!res.test(String(event.target.value).toLowerCase())) {
       setEmailError("Email is incorrect");
     } else {
+      getCurrentData(event.target.value);
       setEmailError("");
     }
   };
@@ -54,6 +94,7 @@ function App() {
     } else if (event.target.value.length > 14) {
       setPasswordError("Password must be less than fourteen characters");
     } else {
+      getCurrentData(event.target.value);
       setPasswordError("");
     }
   };
@@ -63,6 +104,7 @@ function App() {
     if (password !== event.target.value) {
       setRepeatPasswordError("Values do not match");
     } else {
+      getCurrentData(event.target.value);
       setRepeatPasswordError("");
     }
   }
@@ -82,15 +124,12 @@ function App() {
         setRepeatPasswordVisited(true);
         break;
       default:
-        setUsernameVisited(false);
-        setEmailVisited(false);
-        setPasswordVisited(false);
-        setRepeatPasswordVisited(false);
+        alert("Something incomprehensible here :-O");
     }
   };
 
   return (
-    <form class="form" action="" method="post" name="registration_form">
+    <form className="form" name="registration_form" onSubmit={onSubmit}>
       <h1 className="form__title">Register!</h1>
       <ul>
         <li>
@@ -159,11 +198,14 @@ function App() {
         </li>
       </ul>
 
-      <p>
-        Thank you for signing up<span>{username}</span>!
-        We will send you a confirmation e-mail to <span>{email}</span> :)
+      <p className="thanks">
+        Thank you for signing up, <span>{username}</span>! <br />
+        We will send you a confirmation e-mail to <br />
+        <span>{email}</span> :)
       </p>
-    </form>
+      input data:
+      <pre>{JSON.stringify(formData, 0, 2)}</pre>
+    </form >
   );
 }
 
